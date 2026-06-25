06a56268a702662070bd12783de4aafc09822e7a
mathbootcamarhamnestacademy <mathbootcamp2025@gmail.com>
2026-06-25 12:39:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// ArhamNest Academy Starter Script
+
+console.log("ArhamNest Academy Loaded Successfully!");
+
+function startWorksheets() {
+  alert("Worksheet Module Coming Soon!");
+}
+
+function teacherLogin() {
+  alert("Teacher Dashboard will be available in the next update.");
+}
+
+function parentLogin() {
+  alert("Parent Login will be available in the next update.");
+}
